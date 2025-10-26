c8326f2612ace6405097021edb37a9b3ea3e1981
AminHCE <hemmati.c.e@gmail.com>
2025-10-26 17:57:58 +0330

    chore: update settings.py

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -1,5 +1,9 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
